157b50c335ab752253f4c78e918cade41ed36ce9
Saba Azam <sabaazam306@gmail.com>
2020-09-09 00:05:46 +0500

    initial commit

---
+++ client/build/precache-manifest.f8e924734661d3cced9a07597ab7912b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7ead8a674195f737506",
+    "url": "/static/css/main.ea403de4.chunk.css"
+  },
+  {
+    "revision": "a7ead8a674195f737506",
+    "url": "/static/js/main.a7ead8a6.chunk.js"
+  },
+  {
+    "revision": "c9730e402615e5ad07fe",
+    "url": "/static/css/1.90eff2ac.chunk.css"
+  },
+  {
+    "revision": "c9730e402615e5ad07fe",
+    "url": "/static/js/1.c9730e40.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/static/media/login.9a76e74e.svg"
+  },
+  {
+    "revision": "182554625522fac12e37aa67ff7fe21d",
+    "url": "/index.html"
+  }
+];
