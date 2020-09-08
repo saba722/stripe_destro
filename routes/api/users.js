f56d0121e89a3fa0730dbb120e02207925dfb058
Saba Azam <sabaazam306@gmail.com>
2020-09-09 00:17:43 +0500

    initial commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
-const stripe = require('stripe')('pk_test_51HOJDoAsshTLHa3WIeekXFp8tp7N3cjXg7nS5HAwLBeaGpmujBUecSoUVPvyEMMS82sZDB9X3sNqLnPsqlko6XXe00bKOZ3L3h');
+const stripe = require('stripe')('sk_test_51HOJDoAsshTLHa3WTxWQXTePViHBb3mgfS6N9fdgANHdk7CwRQCZBX6m7sUtoDbiaU846cSCmntpJkG2OfpUc1Xv00YgX17eqh');
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
