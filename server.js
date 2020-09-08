d3de84648395ac85aef598b8714510a97ed39e71
Saba Azam <sabaazam306@gmail.com>
2020-09-08 23:59:36 +0500

    initial commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 var cors = require('cors');
 
 const users = require("./routes/api/users");
@@ -14,9 +15,21 @@ app.use(
     extended: false
   })
 );
+
 app.use(bodyParser.json());
 app.use(cors());
 // DB Config
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+//production mode
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
 const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
@@ -33,12 +46,11 @@ app.use(passport.initialize());
 
 // Passport config
 require("./config/passport")(passport);
-
 // Routes
 app.use("/api/users", users);
-app.get('/',(req,res)=>{
-  res.sendFile(__dirname+"/client/build/index.html");
-})
+// app.get('/',(req,res)=>{
+//   res.sendFile(__dirname+"/client/build/index.html");
+// })
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
