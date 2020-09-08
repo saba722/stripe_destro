0ca514d015eeda9943074ab1125006b5667ee5be
Saba Azam <sabaazam306@gmail.com>
2020-09-08 23:17:58 +0500

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/stripe_coupon",
+  mongoURI: "mongodb+srv://stripe:stripe42401@cluster0.93nh8.mongodb.net/stripe_coupon?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
